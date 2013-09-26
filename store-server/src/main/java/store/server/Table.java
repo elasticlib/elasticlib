d27f95b320034c7586bbbf40d9471e70598edb5a
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2013-09-26 22:44:22 +0200

    Refactoring interne de Table.

--- store-server/src/main/java/store/server/Table.java
+++ store-server/src/main/java/store/server/Table.java
@@ -4,10 +4,6 @@ import store.common.Hash;
 
 public abstract class Table<T> {
 
-    private static final char[] ALPHABET = new char[]{'0', '1', '2', '3',
-                                                      '4', '5', '6', '7',
-                                                      '8', '9', 'a', 'b',
-                                                      'c', 'd', 'e', 'f'};
     private final int keyLength;
     private final Object[] buckets;
 
@@ -24,16 +20,9 @@ public abstract class Table<T> {
 
     @SuppressWarnings("unchecked")
     public T get(Hash hash) {
-        int index = indexOf(hash.encode().substring(0, keyLength));
+        String key = hash.encode().substring(0, keyLength);
+        int index = Integer.parseInt(key, 16);
         return (T) buckets[index];
-    }
-
-    private static int indexOf(String key) {
-        int index = 0;
-        for (int i = 0; i < key.length(); i++) {
-            index += pow(key.length() - i - 1) * position(key.charAt(i));
-        }
-        return index;
     }
 
     private static String keyOf(int index, int keyLength) {
@@ -43,18 +32,5 @@ public abstract class Table<T> {
             builder.append('0');
         }
         return builder.append(hex).toString();
-    }
-
-    private static int pow(int i) {
-        return 1 << (4 * i);
-    }
-
-    private static int position(char c) {
-        for (int i = 0; i < ALPHABET.length; i++) {
-            if (ALPHABET[i] == c) {
-                return i;
-            }
-        }
-        throw new IllegalArgumentException(String.valueOf(c));
     }
 }

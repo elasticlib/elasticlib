e09d845fa5e0f9206123d0cf80003b89633f1d5c
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2013-09-28 18:27:08 +0200

    Ajout TODO

--- store-server/src/main/java/store/server/transaction/TransactionContext.java
+++ store-server/src/main/java/store/server/transaction/TransactionContext.java
@@ -3,6 +3,25 @@ package store.server.transaction;
 import java.nio.file.Path;
 
 public interface TransactionContext {
+    //
+    //   \o/   \o/   \o/   \o/   \o/   \o/   \o/   \o/
+    //    |     |     |     |     |     |     |     |
+    //   / \   / \   / \   / \   / \   / \   / \   / \
+    //
+    // TODO ?
+    //
+    // Simplifier l'API :
+    //
+    // - Erreur si ouverture sur un fichier qui n'existe pas (EmptyInput disparait)
+    //
+    // - Ajout méthodes :
+    //      truncate(Path, length)
+    //      exists(Path)
+    //      create(Path)
+    //
+    // - Output toujours en append.
+    // - Surcharge où l'on fourni à l'avance la longueur à écrire (hint pour heavyWrite)
+    //
 
     Input openInput(Path path);
 

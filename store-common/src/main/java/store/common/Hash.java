49ffe8d1dd0c1f645486b1a20c1b20281d2faf79
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2013-11-07 23:53:48 +0100

    Suppression de Uid : n'est plus utilisé.

--- store-common/src/main/java/store/common/Hash.java
+++ store-common/src/main/java/store/common/Hash.java
@@ -1,12 +1,47 @@
 package store.common;
 
-public final class Hash extends AbstractKey {
+import static com.google.common.io.BaseEncoding.base16;
+import java.util.Arrays;
+
+public final class Hash {
+
+    private final byte[] value;
 
     public Hash(byte[] value) {
-        super(value);
+        this.value = value;
     }
 
     public Hash(String encoded) {
-        super(encoded);
+        this(base16()
+                .lowerCase()
+                .decode(encoded.toLowerCase()));
+    }
+
+    public final byte[] value() {
+        return Arrays.copyOf(value, value.length);
+    }
+
+    public final String encode() {
+        return base16()
+                .lowerCase()
+                .encode(value);
+    }
+
+    @Override
+    public final String toString() {
+        return encode();
+    }
+
+    @Override
+    public final int hashCode() {
+        return Arrays.hashCode(value);
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        return Arrays.equals(value, Hash.class.cast(obj).value);
     }
 }

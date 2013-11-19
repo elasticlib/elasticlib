8b007028a9fa88078df4263451ae71af90996aef
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2013-11-19 02:06:32 +0100

    Fusion des implémentations de MessageBodyReader pour Multipart/FormDataMultipart. Corrige au passage la mauvaise prise en charge du type multipart/formData.

--- store-server/src/main/java/store/server/providers/MultipartReader.java
+++ store-server/src/main/java/store/server/providers/MultipartReader.java
@@ -6,16 +6,21 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+import static org.glassfish.jersey.message.internal.MediaTypes.typeEqual;
 import org.jvnet.mimepull.MIMEConfig;
 import org.jvnet.mimepull.MIMEMessage;
+import store.server.multipart.FormDataMultipart;
 import store.server.multipart.Multipart;
 
 /**
  * Multipart reader.
  */
+@Provider
 @Consumes("multipart/*")
 public class MultipartReader implements MessageBodyReader<Multipart> {
 
@@ -34,8 +39,17 @@ public class MultipartReader implements MessageBodyReader<Multipart> {
                               MultivaluedMap<String, String> httpHeaders,
                               InputStream entityStream) throws IOException, WebApplicationException {
 
-        return new Multipart(new MIMEMessage(entityStream,
-                                             mediaType.getParameters().get("boundary"),
-                                             mimeConfig));
+        MIMEMessage mimeMessage = new MIMEMessage(entityStream,
+                                                  mediaType.getParameters().get("boundary"),
+                                                  mimeConfig);
+
+        if (typeEqual(mediaType, MediaType.MULTIPART_FORM_DATA_TYPE)) {
+            // see if the User-Agent header corresponds to some version of MS Internet Explorer
+            // if so, need to set fileNameFix to true to handle issue http://java.net/jira/browse/JERSEY-759
+            String userAgent = httpHeaders.getFirst(HttpHeaders.USER_AGENT);
+            boolean fileNameFix = userAgent != null && userAgent.contains(" MSIE ");
+            return new FormDataMultipart(mimeMessage, fileNameFix);
+        }
+        return new Multipart(mimeMessage);
     }
 }

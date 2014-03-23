878a237e93718780414ad56beaf7ea2035397800
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2014-03-23 12:55:22 +0100

    Retouche sur les tests unitaires du serveur.

--- store-server/src/test/java/store/server/RepositoryTest.java
+++ store-server/src/test/java/store/server/RepositoryTest.java
@@ -6,8 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import static org.fest.assertions.api.Assertions.assertThat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,7 +19,6 @@ import store.server.volume.RevSpec;
  */
 public class RepositoryTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RepositoryTest.class);
     private static final String SOURCE = "source";
     private static final String DESTINATION = "destination";
     private static final Content LOREM_IPSUM = new Content("loremIpsum.txt");
@@ -35,7 +32,6 @@ public class RepositoryTest {
      */
     @BeforeClass
     public void init() throws IOException {
-        LOG.info("Initializing");
         path = Files.createTempDirectory(getClass().getSimpleName() + "-");
         Files.createDirectory(path.resolve("home"));
         repositoriesService = new RepositoriesService(path.resolve("home"), new ReplicationService());
@@ -48,7 +44,6 @@ public class RepositoryTest {
      */
     @AfterClass
     public void cleanUp() throws IOException {
-        LOG.info("Cleaning up");
         recursiveDelete(path);
     }
 
@@ -57,11 +52,15 @@ public class RepositoryTest {
      */
     @Test
     public void createRepositoryTest() {
-        repositoriesService.createRepository(path.resolve(SOURCE));
-        repositoriesService.createRepository(path.resolve(DESTINATION));
+        Path sourcePath = path.resolve(SOURCE);
+        Path destinationPath = path.resolve(DESTINATION);
+
+        repositoriesService.createRepository(sourcePath);
+        repositoriesService.createRepository(destinationPath);
 
         assertThat(repository(SOURCE)).isNotNull();
         assertThat(repository(DESTINATION)).isNotNull();
+        assertThat(repositoriesService.getConfig().getRepositories()).containsExactly(sourcePath, destinationPath);
     }
 
     /**
@@ -70,39 +69,55 @@ public class RepositoryTest {
      * @throws IOException If an IO error occurs.
      */
     @Test(dependsOnMethods = "createRepositoryTest")
-    public void findTest() throws IOException {
-        final Repository source = repository(SOURCE);
+    public void putTest() throws IOException {
+        Repository source = repository(SOURCE);
         try (InputStream inputStream = LOREM_IPSUM.getInputStream()) {
             source.put(LOREM_IPSUM.getInfo(), inputStream, RevSpec.none());
         }
+        assertHas(source, LOREM_IPSUM);
+    }
+
+    /**
+     * Test.
+     */
+    @Test(dependsOnMethods = "putTest")
+    public void createReplicationTest() {
+        repositoriesService.createReplication(SOURCE, DESTINATION);
+
+        assertThat(repositoriesService.getConfig().getSync(SOURCE)).containsExactly(DESTINATION);
         async(new Runnable() {
             @Override
             public void run() {
-                assertThat(source.find("Lorem ipsum", 0, 10)).containsExactly(LOREM_IPSUM.getHash());
+                assertHas(repository(DESTINATION), LOREM_IPSUM);
             }
         });
     }
 
     /**
      * Test.
+     *
+     * @throws IOException If an IO error occurs.
      */
-    @Test(dependsOnMethods = "findTest")
-    public void createReplicationTest() {
-        repositoriesService.createReplication(SOURCE, DESTINATION);
+    @Test(dependsOnMethods = "createReplicationTest")
+    public void findTest() throws IOException {
         async(new Runnable() {
             @Override
             public void run() {
-                List<Event> events = repository(DESTINATION).history(true, 0, 10);
-
-                assertThat(events).hasSize(1);
-                assertThat(events.get(0).getOperation()).isEqualTo(Operation.CREATE);
-                assertThat(events.get(0).getHash()).isEqualTo(LOREM_IPSUM.getHash());
+                assertThat(repository(SOURCE).find("Lorem ipsum", 0, 10)).containsExactly(LOREM_IPSUM.getHash());
             }
         });
     }
 
     private Repository repository(String name) {
         return repositoriesService.getRepository(name);
+    }
+
+    private void assertHas(Repository repository, Content Content) {
+        List<Event> events = repository.history(true, 0, 10);
+
+        assertThat(events).hasSize(1);
+        assertThat(events.get(0).getOperation()).isEqualTo(Operation.CREATE);
+        assertThat(events.get(0).getHash()).isEqualTo(Content.getHash());
     }
 
     private void async(Runnable runnable) {

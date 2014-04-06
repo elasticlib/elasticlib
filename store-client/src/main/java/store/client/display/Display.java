cd91fa71c14865bd117412a4512beb0d28c3a48d
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2014-04-06 23:39:48 +0200

    Utilisation du format YAML pour afficher les résultats dans la console du client.

--- store-client/src/main/java/store/client/display/Display.java
+++ store-client/src/main/java/store/client/display/Display.java
@@ -2,54 +2,71 @@ package store.client.display;
 
 import java.io.PrintWriter;
 import jline.console.ConsoleReader;
-import static store.client.display.FormatUtil.asString;
-import store.common.CommandResult;
-import store.common.ContentInfo;
-import store.common.ContentInfoTree;
-import store.common.Event;
+import static store.client.display.MappableFormatting.format;
+import store.common.Mappable;
+import static store.common.yaml.YamlWriting.write;
 
+/**
+ * Console display interface.
+ */
 public class Display {
 
     private static final String PROMPT = "> ";
     private final ConsoleReader consoleReader;
     private final PrintWriter out;
 
+    /**
+     * Constructor.
+     *
+     * @param consoleReader Console.
+     */
     public Display(ConsoleReader consoleReader) {
         this.consoleReader = consoleReader;
         out = new PrintWriter(consoleReader.getOutput());
         consoleReader.setPrompt(PROMPT);
     }
 
-    public void print(String line) {
-        out.print(line);
+    /**
+     * Print supplied text.
+     *
+     * @param text Text to print.
+     */
+    public void print(String text) {
+        out.print(text);
         out.flush();
     }
 
-    public void println(String line) {
-        out.println(line);
+    /**
+     * Print supplied text and append a line-return.
+     *
+     * @param text Text to print.
+     */
+    public void println(String text) {
+        out.println(text);
         out.flush();
     }
 
-    public void print(ContentInfoTree tree) {
-        println(asString(tree));
+    /**
+     * Print supplied mappable and append a line-return.
+     *
+     * @param mappable Mappable to print.
+     */
+    public void print(Mappable mappable) {
+        println(write(format(mappable)));
     }
 
-    public void print(ContentInfo info) {
-        println(asString(info));
-    }
-
-    public void print(Event event) {
-        println(asString(event));
-    }
-
-    public void print(CommandResult result) {
-        println(asString(result));
-    }
-
+    /**
+     * Set prompt to display.
+     *
+     * @param prompt Prompt to display (excluding actual prompt-separator).
+     */
     public void setPrompt(String prompt) {
         consoleReader.setPrompt(prompt + PROMPT);
     }
 
+    /**
+     * Clear displayed prompt (excluding separator).
+     */
     public void resetPrompt() {
         consoleReader.setPrompt(PROMPT);
     }
---
+++ store-client/src/main/java/store/client/display/MappableFormatting.java
@@ -0,0 +1,96 @@
+package store.client.display;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import store.common.Mappable;
+import store.common.value.Value;
+import store.common.value.ValueType;
+
+/**
+ * Mappable formatting for humans.
+ */
+final class MappableFormatting {
+
+    private static final String TRANSACTION_ID = "transactionId";
+    private static final String LENGTH = "length";
+    private static final Collection<String> REMOVALS = Arrays.asList(TRANSACTION_ID);
+    private static final Map<ValueType, Formatter> FORMATTERS = new EnumMap<>(ValueType.class);
+
+    static {
+        FORMATTERS.put(ValueType.DATE, new Formatter() {
+            @Override
+            public Value apply(Value value) {
+                return Value.of(DateTimeFormat
+                        .longDateTime()
+                        .withZone(DateTimeZone.getDefault())
+                        .print(value.asInstant()));
+            }
+        });
+        FORMATTERS.put(ValueType.HASH, new Formatter() {
+            @Override
+            public Value apply(Value value) {
+                return Value.of(value.asHash().asHexadecimalString());
+            }
+        });
+        FORMATTERS.put(ValueType.OBJECT, new Formatter() {
+            @Override
+            public Value apply(Value value) {
+                Map<String, Value> map = new LinkedHashMap<>();
+                for (Entry<String, Value> entry : value.asMap().entrySet()) {
+                    map.put(entry.getKey(), formatValue(entry.getValue()));
+                }
+                return Value.of(map);
+            }
+        });
+        FORMATTERS.put(ValueType.ARRAY, new Formatter() {
+            @Override
+            public Value apply(Value value) {
+                List<Value> list = new ArrayList<>();
+                for (Value item : value.asList()) {
+                    list.add(formatValue(item));
+                }
+                return Value.of(list);
+            }
+        });
+    }
+
+    private interface Formatter {
+
+        public Value apply(Value value);
+    }
+
+    private MappableFormatting() {
+    }
+
+    public static Map<String, Value> format(Mappable mappable) {
+        Map<String, Value> map = new LinkedHashMap<>();
+        for (Entry<String, Value> entry : mappable.toMap().entrySet()) {
+            String key = entry.getKey();
+            Value value = entry.getValue();
+            if (REMOVALS.contains(key)) {
+                continue;
+            }
+            if (key.equals(LENGTH)) {
+                map.put(key, Value.of(ByteLengthFormatter.format(entry.getValue().asLong())));
+                continue;
+            }
+            map.put(key, formatValue(value));
+        }
+        return map;
+    }
+
+    private static Value formatValue(Value value) {
+        if (!FORMATTERS.containsKey(value.type())) {
+            return value;
+        }
+        return FORMATTERS.get(value.type()).apply(value);
+    }
+}

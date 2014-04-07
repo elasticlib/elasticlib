cd612c976d2f76025d91f8ca28b2b072f1536721
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2014-04-08 00:42:35 +0200

    Ajout de javadoc.

--- store-client/src/main/java/store/client/exception/QuitException.java
+++ store-client/src/main/java/store/client/exception/QuitException.java
@@ -1,5 +1,8 @@
 package store.client.exception;
 
+/**
+ * Thrown to indicates that this client has to stop its execution.
+ */
 public class QuitException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
--- store-client/src/main/java/store/client/exception/RequestFailedException.java
+++ store-client/src/main/java/store/client/exception/RequestFailedException.java
@@ -2,12 +2,27 @@ package store.client.exception;
 
 import java.io.IOException;
 
+/**
+ * Thrown if a requests fails for any reason.
+ */
 public class RequestFailedException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructor.
+     *
+     * @param message Error message.
+     */
     public RequestFailedException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param e The cause of this exception.
+     */
     public RequestFailedException(IOException e) {
         super(e.getMessage());
     }

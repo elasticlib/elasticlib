863e8088efd3381dd9a722ba49e373ed62117bba
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2014-05-02 15:21:10 +0200

    Commande 'put' : ajout du support des dossiers.

--- store-client/src/main/java/store/client/command/Put.java
+++ store-client/src/main/java/store/client/command/Put.java
@@ -1,10 +1,18 @@
 package store.client.command;
 
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 import java.util.List;
 import store.client.config.ClientConfig;
 import store.client.display.Display;
+import store.client.exception.RequestFailedException;
 import store.client.http.Session;
 import store.client.util.Directories;
 import store.common.CommandResult;
@@ -21,10 +29,39 @@ class Put extends AbstractCommand {
     }
 
     @Override
-    public void execute(Display display, Session session, ClientConfig config, List<String> params) {
-        String repository = session.getRepository();
+    public void execute(final Display display, final Session session, ClientConfig config, List<String> params) {
+        final String repository = session.getRepository();
         Path path = Directories.resolve(Paths.get(params.get(0)));
-        CommandResult result = session.getClient().put(repository, path);
-        display.print(result);
+        if (!Files.exists(path)) {
+            throw new RequestFailedException("Does not exist");
+        }
+        if (!Files.isDirectory(path)) {
+            CommandResult result = session.getClient().put(repository, path);
+            display.print(result);
+            return;
+        }
+        try {
+            Files.walkFileTree(path,
+                               EnumSet.of(FileVisitOption.FOLLOW_LINKS),
+                               Integer.MAX_VALUE,
+                               new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
+                    if (attributes.isRegularFile()) {
+                        display.println("Processing: " + file);
+                        try {
+                            CommandResult result = session.getClient().put(repository, file);
+                            display.print(result);
+
+                        } catch (RequestFailedException e) {
+                            display.print(e);
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            throw new RequestFailedException(e);
+        }
     }
 }

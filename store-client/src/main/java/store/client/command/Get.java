17aed0e9596f514c498e533cb0873f326e88df5b
Guillaume Masclet <guillaume.masclet@yahoo.fr>
2014-10-28 23:05:11 +0100

    Ajout d'un feedback de progression du téléchargement sur la commande Get.

--- store-client/src/main/java/store/client/command/Get.java
+++ store-client/src/main/java/store/client/command/Get.java
@@ -1,17 +1,17 @@
 package store.client.command;
 
-import com.google.common.base.Optional;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import static java.nio.file.Files.newOutputStream;
 import java.nio.file.Path;
 import java.util.List;
 import store.client.config.ClientConfig;
 import store.client.display.Display;
+import store.client.display.LoggingInputStream;
 import store.client.exception.RequestFailedException;
 import store.client.http.Session;
 import static store.client.util.ClientUtil.parseHash;
+import store.client.util.DefferedFileOutputStream;
 import store.client.util.Directories;
 import store.common.client.Content;
 import store.common.hash.Hash;
@@ -34,70 +34,18 @@ class Get extends AbstractCommand {
         try {
             try (Content content = session.getRepository().getContent(hash)) {
                 Path path = Directories.resolve(content.getFileName().or(hash.asHexadecimalString()));
-                try (InputStream inputStream = content.getInputStream();
+                try (InputStream inputStream = new LoggingInputStream(display,
+                                                                      config,
+                                                                      "Downloading",
+                                                                      content.getInputStream(),
+                                                                      content.getLength());
                         OutputStream outputStream = new DefferedFileOutputStream(path)) {
+
                     copy(inputStream, outputStream);
                 }
             }
         } catch (IOException e) {
             throw new RequestFailedException(e);
-        }
-    }
-
-    /**
-     * A file output stream that create or truncate file in a lazy way, only when first byte is written.
-     */
-    private static class DefferedFileOutputStream extends OutputStream {
-
-        private final Path path;
-        private Optional<OutputStream> outputStream;
-
-        /**
-         * Constructor.
-         *
-         * @param path Path to write to.
-         */
-        public DefferedFileOutputStream(Path path) {
-            this.path = path;
-            outputStream = Optional.absent();
-        }
-
-        @Override
-        public void write(int b) throws IOException {
-            if (!outputStream.isPresent()) {
-                outputStream = Optional.of(newOutputStream(path));
-            }
-            outputStream.get().write(b);
-        }
-
-        @Override
-        public void write(byte[] buf) throws IOException {
-            write(buf, 0, buf.length);
-        }
-
-        @Override
-        public void write(byte[] buf, int off, int len) throws IOException {
-            if (len == 0) {
-                return;
-            }
-            if (!outputStream.isPresent()) {
-                outputStream = Optional.of(newOutputStream(path));
-            }
-            outputStream.get().write(buf, off, len);
-        }
-
-        @Override
-        public void flush() throws IOException {
-            if (outputStream.isPresent()) {
-                outputStream.get().flush();
-            }
-        }
-
-        @Override
-        public void close() throws IOException {
-            if (outputStream.isPresent()) {
-                outputStream.get().close();
-            }
         }
     }
 }
--- store-client/src/main/java/store/client/command/Put.java
+++ store-client/src/main/java/store/client/command/Put.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 import store.client.config.ClientConfig;
 import store.client.display.Display;
+import store.client.display.LoggingInputStream;
 import store.client.exception.RequestFailedException;
 import store.client.http.Session;
 import static store.client.util.ClientUtil.revisions;
@@ -228,115 +229,6 @@ class Put extends AbstractCommand {
                 }
             }
             return FileVisitResult.CONTINUE;
-        }
-    }
-
-    /**
-     * An input stream that prints read progression.
-     */
-    private static class LoggingInputStream extends InputStream {
-
-        private static final int EOF = -1;
-        private final Display display;
-        private final ClientConfig config;
-        private final String task;
-        private InputStream inputStream;
-        private final long length;
-        private long read;
-        private int currentProgress;
-        private boolean closed;
-
-        /**
-         * Constructor.
-         *
-         * @param display Display.
-         * @param config Config.
-         * @param task Task description to print.
-         * @param inputStream Underlying input-stream.
-         * @param length Expected total length to read.
-         */
-        public LoggingInputStream(Display display, ClientConfig config, String task, InputStream inputStream,
-                                  long length) {
-            this.display = display;
-            this.config = config;
-            this.task = task;
-            this.inputStream = inputStream;
-            this.length = length;
-        }
-
-        private void increment(long n) {
-            read += n;
-            int newProgress = (int) ((read * 100.0d) / length);
-            if (newProgress != currentProgress) {
-                currentProgress = newProgress;
-                if (config.isDisplayProgress()) {
-                    display.print(task + " " + currentProgress + "%\r");
-                }
-            }
-        }
-
-        @Override
-        public int read() throws IOException {
-            int res = inputStream.read();
-            if (res != EOF) {
-                increment(1);
-            }
-            return res;
-        }
-
-        @Override
-        public int read(byte[] b) throws IOException {
-            return read(b, 0, b.length);
-        }
-
-        @Override
-        public int read(byte[] b, int off, int len) throws IOException {
-            int res = inputStream.read(b, off, len);
-            if (res != EOF) {
-                increment(res);
-            }
-            return res;
-        }
-
-        @Override
-        public long skip(long n) throws IOException {
-            increment(n);
-            return inputStream.skip(n);
-        }
-
-        @Override
-        public int available() throws IOException {
-            return inputStream.available();
-        }
-
-        @Override
-        public boolean markSupported() {
-            return false;
-        }
-
-        @Override
-        public synchronized void mark(int readlimit) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public synchronized void reset() throws IOException {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void close() throws IOException {
-            if (!closed) {
-                if (config.isDisplayProgress()) {
-                    StringBuilder builder = new StringBuilder();
-                    for (int i = 0; i < task.length() + " 100%".length(); i++) {
-                        builder.append(" ");
-                    }
-                    builder.append('\r');
-                    display.print(builder.toString());
-                }
-                inputStream.close();
-            }
         }
     }
 }
